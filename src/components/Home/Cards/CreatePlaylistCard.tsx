bac3d061bd86e9b99e6598618be593944935619d
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-06-19 13:54:55 -0300

    style: decrease CreateFirstPlaylist button size

--- src/components/Home/Cards/CreatePlaylistCard.tsx
+++ src/components/Home/Cards/CreatePlaylistCard.tsx
@@ -12,17 +12,17 @@ export const CreatePlaylistCard = ({ className }: CreatePlaylistCardProps) => {
     <CreatePlaylist>
       <Button
         variant="ghost"
-        className={cn("h-fit p-3 flex gap-3 items-center shrink-0 rounded-md transition-colors", className)}
+        className={cn("h-fit p-3 pr-5 flex gap-3 items-center shrink-0 rounded-2xl transition-colors", className)}
         title="Crie sua primeira playlist!"
       >
         <div
-          className={cn("shrink-0 size-44 aspect-square bg-zinc-600 grid place-content-center rounded-2xl", className)}
+          className={cn("shrink-0 size-32 aspect-square bg-zinc-600 grid place-content-center rounded-xl", className)}
         >
-          <FaPlus size={120} className="text-zinc-300" />
+          <FaPlus size={92} className="text-zinc-300" />
         </div>
 
         <div className="flex flex-col gap-2.5 text-left max-w-full">
-          <span className="text-2xl font-medium leading-snug">
+          <span className="text-xl font-medium leading-snug">
             Crie sua primeira playlist!
           </span>
         </div>
